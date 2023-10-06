08954803205736c8a4419911f564877134f12dc2
ShresthaSudina <sudina.shrestha2001@gmail.com>
2023-10-06 06:19:16 +0545

    Project finalization

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php include 'inc/header.php';?>
+<style>
+.notfound{}	
+.notfound h2{font-size: 80px;line-height: 130px;text-align: center;}
+.notfound h2 span{display: block;color: black;font-size: 170px;}
+</style>
+
+ <div class="main">
+    <div class="content">
+    	<div class="section group">
+    		<div class="notfound">
+    			<h2><span>Product</span> Not Found</h2>
+    		</div>
+    	</div>
+    	
+    	 	
+       <div class="clear"></div>
+    </div>
+ </div>
+<?php include 'inc/footer.php';?>
---
+++ inc/footer.php
@@ -0,0 +1,72 @@
+</div>
+   <div class="footer">
+   	  <div class="wrapper">	
+	     <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+						<h4>Information</h4>
+						<ul>
+						<li><a href="contact.php">Customer Service</span></a></li>
+					
+						<li><a href="contact.php"><span>Contact Us</span></a></li>
+						</ul>
+					</div>
+				
+				<div class="col_1_of_4 span_1_of_4">
+					<h4>My account</h4>
+						<ul>
+							<li><a href="login.php">Sign In</a></li>
+							<li><a href="cart.php">View Cart</a></li>
+							<li><a href="contact.php">Help</a></li>
+						</ul>
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h4>Contact</h4>
+						<ul>
+							<li><span>+977 9808910898</span></li>
+						
+						</ul>
+						<div class="social-icons">
+							<h4>Follow Us</h4>
+					   		  <ul>
+							      <li class="facebook"><a href="#" target="_blank"> </a></li>
+							      <li class="twitter"><a href="#" target="_blank"> </a></li>
+							      <li class="googleplus"><a href="#" target="_blank"> </a></li>
+							      <li class="contact"><a href="#" target="_blank"> </a></li>
+							      <div class="clear"></div>
+						     </ul>
+   	 					</div>
+				</div>
+			</div>
+			
+		
+			
+		
+     </div>
+    </div>
+    <script type="text/javascript">
+		$(document).ready(function() {
+		
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+    <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+    <link href="css/flexslider.css" rel='stylesheet' type='text/css' />
+	  <script defer src="js/jquery.flexslider.js"></script>
+	  <script type="text/javascript">
+		$(function(){
+		  SyntaxHighlighter.all();
+		});
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	  </script>
+
+</body>
+</html>
